2f2398eee53e9575f8ca1573e9b6aaeb77435738
Jeff <jeff@fgeth.com>
2023-04-28 08:03:22 -0500

    moved vars to Transaction

--- appData.go
+++ appData.go
@@ -32,12 +32,7 @@ var (
 	TxDir  =	"tx"
 	ItemsDir =	"items"
 	
-	//setup map of transactions
-	Transactions		map[uint64]Transaction			 //Known Transactions
-	notValid := 		make(map[uint64][]transaction)   //Map of potential transactions ID to node IDs indicating a bad transaction was approved. We do not track valid transaction in a map that is track in each bundle, but this is a way to track and vote a bad transaction be reversed and removed.
-	authorized := 		make(map[uint64][]uint64)  		 //Map of bundle IDs to a list of Authorized Node IDs
-
-	
+		
 	//setup Mutex to lock global variable to ensure app stability
 	Mu sync.Mutex
 )
